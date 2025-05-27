c0823a48039b808fcf3c8da2057fc8b21fff212b
stanqb <9857@holbertonstudents.com>
2025-05-27 13:31:12 +0200

    Task 9: Implement namespace with declaration merging for subjects
    - Create Subjects namespace with Teacher interface and Subject class
    - Use declaration merging to extend Teacher interface in each subject
    - Implement Cpp, React, and Java classes with specific requirements
    - Add subject-specific teacher availability checks based on experience.

---
+++ TypeScript/task_4/dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+
+// Empty main file for webpack compilation
+console.log("Task 4 - Namespace & Declaration merging");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLDBDQUEwQztBQUMxQyxPQUFPLENBQUMsR0FBRyxDQUFDLDBDQUEwQyxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90eXBlc2NyaXB0X2RlcGVuZGVuY2llcy8uL2pzL21haW4udHMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gRW1wdHkgbWFpbiBmaWxlIGZvciB3ZWJwYWNrIGNvbXBpbGF0aW9uXG5jb25zb2xlLmxvZyhcIlRhc2sgNCAtIE5hbWVzcGFjZSAmIERlY2xhcmF0aW9uIG1lcmdpbmdcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ TypeScript/task_4/js/main.ts
@@ -0,0 +1,2 @@
+// Empty main file for webpack compilation
+console.log("Task 4 - Namespace & Declaration merging");
